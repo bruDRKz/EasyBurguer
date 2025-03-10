f9820138d0e39ae031fb5fb27db9f240986b6fa8
Bruno Rodrigues <br.rodrigues373@gmail.com>
2025-03-10 17:53:59 -0300

    Commit pre migrations

--- EasyBurguer/Models/Categoria.cs
+++ EasyBurguer/Models/Categoria.cs
@@ -1,11 +1,26 @@
-﻿namespace EasyBurguer.Models
+﻿using Microsoft.Extensions.WebEncoders.Testing;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyBurguer.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaID { get; set; }
+  //-------------------------------------------------------------------------------------
+        [StringLength(100, ErrorMessage ="O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="O campo é obrigatório!")]
+        [Display(Name ="Nome")]
         public string CategoriaNome { get; set; }
+ //--------------------------------------------------------------------------------
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a Descrição da Categoria!")]
+        [Display(Name = "Descrição")]
         public string Descricao { get; set; }
 
         public List<Lanche> Lanches {get; set;}
+        
     }
 }
--- EasyBurguer/obj/Debug/net8.0/EasyBurguer.AssemblyInfo.cs
+++ EasyBurguer/obj/Debug/net8.0/EasyBurguer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyBurguer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c194b174fcff1fbe50969127961836685286c5e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyBurguer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyBurguer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
