49ce932145d20f69fe1df95387dda2f57a7bffa8
Bruno Carvalho Rodrigues <br.rodrigues373@gmail.com>
2025-07-01 20:26:04 -0300

    Ajustes de branch e Contatos

---
+++ EasyBurguer/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,30 @@
+﻿using EasyBurguer.Models;
+using EasyBurguer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBurguer.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+    }
+    
+}
---
+++ EasyBurguer/Controllers/LancheController.cs
@@ -0,0 +1,53 @@
+﻿using AspNetCoreGeneratedDocument;
+using EasyBurguer.Models;
+using EasyBurguer.Repositories;
+using EasyBurguer.Repositories.Interfaces;
+using EasyBurguer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBurguer.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _LancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository) //Injeção de dependencia do meu repositorio para acesso ao banco
+        {
+            _LancheRepository = lancheRepository;
+        }
+
+        public IActionResult List(string categoria)
+        {
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual = string.Empty;
+
+            if (string.IsNullOrEmpty(categoria))
+            {
+                lanches = _LancheRepository.Lanches.OrderBy(l => l.LancheID);
+                categoriaAtual = "Todos os lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _LancheRepository.Lanches
+                        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                        .OrderBy(l => l.Nome);
+                }
+                else 
+                {
+                    lanches = _LancheRepository.Lanches
+                       .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                       .OrderBy(l => l.Nome);
+                }
+                    categoriaAtual = categoria;
+            }
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual,
+            };
+                return View(lanchesListViewModel);
+        }
+    }
+}
